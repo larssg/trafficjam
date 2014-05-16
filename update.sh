d76429ce4bdd42753f1bc46695a817e04b3eabfc
mads hobye <madshobye@madss-MacBook-Pro-2.local>
2014-05-16 11:58:27 +0200

    just update

--- update.sh
+++ update.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 git add --all
 git commit -m 'just update'
-git pull 
+git pull https://github.com/larssg/trafficjam.git
+git pull git@heroku.com:trafficjam-demo.git
 git push https://github.com/larssg/trafficjam.git
 git push git@heroku.com:trafficjam-demo.git
+
+# to quite vim: esc + :wq
